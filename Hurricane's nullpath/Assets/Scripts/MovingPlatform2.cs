c3f18d9ff4eb41a71809fe20a1ee7528e683f8f6
Boitumelo274 <2878260@students.wits.ac.za>
2025-08-17 07:57:40 +0200

    stage3 done

---
+++ Hurricane's nullpath/Assets/Scripts/MovingPlatform2.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MovingPlatform2 : MonoBehaviour
+{
+
+    [SerializeField] string playerTag = "Player";
+    [SerializeField] Transform platform;
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag.Equals(playerTag))
+        {
+            collision.gameObject.transform.parent = platform;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag.Equals(playerTag))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
