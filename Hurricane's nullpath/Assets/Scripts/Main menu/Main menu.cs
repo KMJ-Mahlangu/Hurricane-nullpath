09da7d0e575738d200e90d0b78f341d77276fd78
KMJ-Mahlangu <kmjkamogelo@gmail.com>
2025-10-18 05:46:45 +0200

    Main menu

--- Hurricane's nullpath/Assets/Scripts/Main menu/Main menu.cs
+++ Hurricane's nullpath/Assets/Scripts/Main menu/Main menu.cs
@@ -1,14 +1,41 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Mainmenu : MonoBehaviour
 {
-   
-    public void Play()
+    public GameObject MainMenuCanvas;
+    public GameObject FirstSelectedButton;
+
+    public MonoBehaviour PlayerScript;
+
+    private void Start()
     {
-        SceneManager.LoadScene(1);
+        
+        CursorVisible(true);
+
+        if (PlayerScript != null)
+            PlayerScript.enabled = false;
+
+        if (FirstSelectedButton != null)
+            EventSystem.current.SetSelectedGameObject(FirstSelectedButton);
     }
 
+    public void Play()
+    {
+        if (MainMenuCanvas !=null)
+            MainMenuCanvas.SetActive(true);
+        CursorVisible(false);
+       
+        if (PlayerScript!=null)
+            PlayerScript.enabled = true;
+    }
+
+    private void CursorVisible(bool show)
+    {
+        Cursor.visible = show;
+        Cursor.lockState = show ? CursorLockMode.None : CursorLockMode.Locked;
+    }
     public void Quit()
     {
         Application.Quit();
@@ -16,6 +43,6 @@ public class Mainmenu : MonoBehaviour
 
     public void MainMenu()
     {
-        SceneManager.LoadScene(0);
+        //SceneManager.LoadScene(0);
     }
 }
